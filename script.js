c43bc2e72b362973eeb9afe5934bc79fb4eac0e2
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-12-02 15:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const panel = document.querySelectorAll(".panel");
+panel.forEach((img)=>{
+	img.addEventListener("click",()=>{
+		removeElement();
+		img.classList.add("active");
+	})
+})
+
+function removeElement() {
+   panel.forEach((e)=>{
+	   e.classList.remove("active");
+   })	
+}
+
+
